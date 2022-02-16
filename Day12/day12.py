c98beccb222f51f017ecf1d693816c8c0572ecfe
Bruce Felix <brucefelixm@gmail.com>
2022-02-16 13:13:11 +0300

    Two exercises down 1 more to go

--- Day12/day12.py
+++ Day12/day12.py
@@ -26,3 +26,38 @@ def rgb_color_gen():
     val3 = random.randint(0, 255)
     return "rgb({},{},{})".format(val1,val2,val3)
 print(rgb_color_gen())
+
+
+#Exercises: Level 2
+# 1 generating hexadecimal colors
+def list_of_hexa_colors(numb_of_colors):
+    colors = []
+    letters = random.choices("ABCDEF", k=6)
+    digits = random.choices("1234567890", k = 6)
+    for color in range(1, numb_of_colors + 1):
+        color =  "#" + ''.join(random.sample(digits + letters, 6))
+        colors.append(color)
+    print(colors)
+
+# 2 generating rgb colors
+def list_of_rgb_colors(numb_of_colors):
+    colors = []
+    val1 = random.randint(0, 255)
+    val2 = random.randint(0, 255)
+    val3 = random.randint(0, 255)
+    for color in range(1, numb_of_colors + 1):
+        color = "rgb({},{},{})".format(val1,val2,val3)
+        colors.append(color)
+    print(colors)
+
+# 3 hexa or rgb generator
+def hex_or_rgb():
+    type = input("Enter the color type you need: rgb or hexa: ")
+    number = int(input("Enter the number of colors you need: "))
+    if type.lower() == "hexa":
+        list_of_hexa_colors(number)
+    elif type.lower() == "rgb":
+        list_of_rgb_colors(number)
+    else:
+        print("Invalid type of color")
+hex_or_rgb()
