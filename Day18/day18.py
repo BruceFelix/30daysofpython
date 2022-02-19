55ba24b3aefe02e2feaa15b728b6d91ea7f7feb2
Bruce Felix <brucefelixm@gmail.com>
2022-02-19 16:36:57 +0300

    day 18 in progress

--- Day18/day18.py
+++ Day18/day18.py
@@ -2,16 +2,15 @@
 import re
 
 # 1 Extract values and return their differences
-# points = ['-1', '2', '-4', '-3', '-1', '0', '4', '8']
-# sorted_points =  [-4, -3, -1, -1, 0, 2, 4, 8]
-# distance = 8 -(-4) # 12
-# first_value = re.findall('^-4', points)
-# last_value = re.findall('8&', points)
-# print(int(last_value) - int(first_value))
+sorted_points =  [-4, -3, -1, -1, 0, 2, 4, 8]
+distance = 8 -(-4) # 12
+first_value = re.findall('^-4', sorted_points)
+last_value = re.findall('8&', sorted_points)
+print(last_value[0] - first_value[0])
 
 #2 valid python variable
-def is_valid_variable(text):
-    pattern = r"A-Za-z"
-    result = re.findall(pattern, text)
-    print(result)
-is_valid_variable("Hello")
+# def is_valid_variable(text):
+#     pattern = r"A-Za-z"
+#     result = re.findall(pattern, text)
+#     print(result)
+# is_valid_variable("Hello")
