e7ff0e16553c6f5f00d83ea912d0e5890fff81b1
Bruce Felix <brucefelixm@gmail.com>
2022-03-10 10:47:11 +0300

    stuck in qeustion 2

--- Day19/day19.py
+++ Day19/day19.py
@@ -3,42 +3,33 @@
 import json
 #Exercises: Level 1
 #1 counts the number of lines and words in the 4 speeches
-def words_lines_numbers(file):
-    number_of_words = 0
-    number_of_lines = 0
-    with open(file) as speeches:
-        num_lines = speeches.readlines()
+# def words_lines_numbers(file):
+#     number_of_words = 0
+#     number_of_lines = 0
+#     with open(file) as speeches:
+#         num_lines = speeches.readlines()# Reads the files line by line
 
-        for lines in num_lines:
-            number_of_lines += len(lines.split('.'))
+#         print(f'There are {len(num_lines)} lines in {file}')
 
-        print(f'There are {number_of_lines} lines in {file}')
+#         for line in num_lines:
+#             number_of_words += len(line.split(" "))
 
-        for line in num_lines:
-            number_of_words += len(line.split())
-
-        print(f'There are {number_of_words} of words in {file}')
-words_lines_numbers('obama_speech.txt')
-words_lines_numbers('michelle_obama_speech.txt')
-words_lines_numbers('donald_speech.txt')
-words_lines_numbers('melina_trump_speech.txt')
+#         print(f'There are {number_of_words} of words in {file}')
+# words_lines_numbers('obama_speech.txt')
+# words_lines_numbers('michelle_obama_speech.txt')
+# words_lines_numbers('donald_speech.txt')
+# words_lines_numbers('melina_trump_speech.txt')
 
 
 #2 most spoken languages
-import json
 # python dictionary
-person = {
-    "name": "Asabeneh",
-    "country": "Finland",
-    "city": "Helsinki",
-    "skills": ["JavaScrip", "React", "Python"]
-}
-# let's convert it to  json
-person_json = json.dumps(person, indent=4) # indent could be 2, 4, 8. It beautifies the json
-print(type(person_json))
-print(person_json)
-def ten_most_spoken_words(file, number):
-    with open(file) as speakers:
-        all_languages = speakers.readlines()
-        languages = json.loads(all_languages)
-        print(languages)
+def ten_most_spoken_words():
+    pass
+# ten_most_spoken_words()
+# with open('countries_data.json', 'r') as speakers:
+#     languages = json.load(speakers)
+#     print(languages)
+with open("countries_data.json", 'r') as speakers:
+    languages = json.load(speakers)
+    print(languages)
+    
