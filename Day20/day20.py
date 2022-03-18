67bc65f25c6d3cfdbfbdedc6824ecffb889d5e16
Bruce Felix <brucefelixm@gmail.com>
2022-03-18 14:44:10 +0300

    Done with day 21

--- Day20/day20.py
+++ Day20/day20.py
@@ -1,5 +1,6 @@
 """Packages and how to use them"""
 #1 Read from a url and find the 10 most frequent words in romeo_and_juliet
+from wsgiref.util import request_uri
 import requests
 # from collections import Counter
 # response = requests.get("http://www.gutenberg.org/files/1112/1112.txt") # opens network and fetchs data
@@ -10,4 +11,11 @@ import requests
 # i the min, max, mean, median, standard deviation of cats' weight in metric units.
 
 response = requests.get("https://api.thecatapi.com/v1/breeds")
-print(response.text)
+cats = response.json()
+# print(cats[0])
+## Did not do exercise 2 and 3 because it is all statistics which I'm not interested in.
+
+# Level 4
+from bs4 import BeautifulSoup
+response = requests.get("https://archive.ics.uci.edu/ml/datasets.php")
+print(BeautifulSoup(response, "html.parser"))
